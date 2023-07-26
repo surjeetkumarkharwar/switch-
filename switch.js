86dbceb0d2b0855b0a32cde4e5011cdad9a0807d
surjeetkumarkharwar <140419537+surjeetkumarkharwar@users.noreply.github.com>
2023-07-26 12:31:33 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,12 @@
+var num =20;
+switch (num)
+{
+    case 10:
+    console.log("number is :10");
+    break ;
+    case 20:
+    console.log("number is :20");
+    break ;
+    default:
+    console.log("this is the default block");
+}
